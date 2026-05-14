88549667ffc0d9111190111f2a72dc643e1f9bd7
tehseekers <tehseekers@gmail.com>
2026-05-15 01:05:55 +0800

    Update header logo to PNG

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import gallery1 from './assets/gallery-1.jpg';
 import gallery2 from './assets/gallery-2.jpg';
 import gallery3 from './assets/gallery-3.jpg';
 import gallery4 from './assets/gallery-4.jpg';
-import logoImage from './assets/logo.svg';
+import logoImage from './assets/image.png';
 
 function setImageSources() {
   document.getElementById('hero-img').src = heroImage;
